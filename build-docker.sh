e634dc001f593ee70f55d168d1d5ec7993c254a5
minghongg <minghong2403@gmail.com>
2022-01-12 11:24:14 +0700

    Fix build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,6 @@
 docker rm $(docker ps -a -q) --force
-docker build -t apisploit .
+if test -z "$(docker images -q apisploit:latest)"; then
+    docker build -t apisploit .
+fi
 docker run -dit -p 8000:8000 --name testapisploit apisploit
 docker exec -it testapisploit bash
